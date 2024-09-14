b416376fec8d9d6cad639d6e1eb7e2d047c86fed
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-09-14 05:04:40 -0300

    feat: add registerSchema from zod

--- app/components/form-login.tsx
+++ app/components/form-login.tsx
@@ -1,9 +1,13 @@
 "use client"
 
+import { loginSchema } from "@/schemas/loginSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form"
+
 const FormLogin = () => {
     return (
         <div>FormLogin</div>
-      );
+    );
 }
- 
+
 export default FormLogin;
---
+++ schemas/registerSchema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$/;
+
+export const registerSchema = z.object({
+    name: z.string({ required_error: "Nome é obrigatório" }).min(1, "Nome é obrigatório").max(32, "Nome não pode ser maior que 32 caracteres"),
+    email: z.string({ required_error: "Email é obrigatório" }).email("Email inválido"),
+    password: z.string({ required_error: "Senha é obrigatória" }).regex(passwordRegex, "A senha deve conter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma letra minúscula, um número e um caractere especial")
+})
