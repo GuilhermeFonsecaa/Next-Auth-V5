a6ce82b3707ac06c0af12e293253617bc4b9984f
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-09-16 03:44:03 -0300

    feat: add auth.js and prisma ORM config

--- app/(auth)/(protected)/dashboard/page.tsx
+++ app/(auth)/(protected)/dashboard/page.tsx
@@ -1,7 +1,15 @@
-const Dashboard = () => {
-    return ( 
-        <div>Dashboard</div>
-     );
+import { auth } from "@/auth";
+
+export default async function DashboardPage() {
+    const session = await auth();
+
+    if (!session) {
+        return <div>Não autenticado</div>
+    }
+
+    return (
+        <div className="container">
+            <pre>{JSON.stringify(session, null, 2)}</pre>
+        </div>
+    )
 }
- 
-export default Dashboard;
