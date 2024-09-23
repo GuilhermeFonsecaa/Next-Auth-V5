bdf2a26eccac130a2cf9c832f25d972263d22f08
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-09-23 03:24:07 -0300

    feat: add protected routes middleware config

--- app/(auth)/(protected)/admin/page.tsx
+++ app/(auth)/(protected)/admin/page.tsx
@@ -1,7 +1,19 @@
-const AdminPage = () => {
-    return (  
-        <div>AdminPage</div>
-    );
+import LogoutButton from "@/app/components/logout-button";
+import { auth } from "@/auth";
+
+const AdminPage = async () => {
+    const session = await auth();
+
+    if (session?.user?.role !== "ADMIN") {
+        return <div>Você não é um administrador</div>
+    }
+
+    return (
+        <div className="container">
+            <pre>{JSON.stringify(session, null, 2)}</pre>
+            <LogoutButton />
+        </div>
+    )
 }
- 
+
 export default AdminPage;
