d76e07db6c7557312c2f73558ca3e0ea1d9eb806
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-09-23 00:43:26 -0300

    feat: add auth config

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,38 @@
 import type { NextAuthConfig } from "next-auth";
+import { loginSchema } from "./schemas/loginSchema";
+import Credentials from "next-auth/providers/credentials"
+import { db } from "./lib/db";
+import bcrypt from "bcryptjs"
 
 export default {
-    providers: [],
+    providers: [
+        Credentials({
+            authorize: async (credentials) => {
+                const { data, success } = loginSchema.safeParse(credentials);
+
+                if (!success) {
+                    throw new Error("Credenciais inválidas");
+                }
+
+                const user = await db.user.findUnique({
+                    where: {
+                        email: data.email,
+                    },
+                });
+
+                if (!user || !user.password) {
+                    throw new Error("Usuário não encontrado");
+                }
+
+                const isValid = await bcrypt.compare(data.password, user.password);
+
+                if(!isValid) {
+                    throw new Error("Senha incorreta")
+                }
+
+                return user;
+
+            }
+        })
+    ],
 } satisfies NextAuthConfig;
