eee10fa0b4a292b66d18cb2f1d45a851866fbd09
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-09-23 03:22:12 -0300

    feat: add role support to NextAuth sessions and JWT

--- auth.ts
+++ auth.ts
@@ -7,5 +7,24 @@ import { db } from "@/lib/db";
 export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: PrismaAdapter(db),
     session: { strategy: "jwt" },
-    ...authConfig
+    ...authConfig,
+    callbacks: {
+        
+        //jwt() executa cada vez que cria ou atualiza um token JWT
+        //Onde se pode adicionar informações ao token
+        jwt({ token, user }) {
+            if (user) {
+               token.role = user.role;
+            }
+            return token;
+        },
+
+        //session() é utilizado para adicionar a informação do token a sessão do usuário, deixando disponível no cliente
+        session({ session, token }) {
+            if(session.user) {
+                session.user.role = token.role;
+            }
+            return session;
+        }
+    }
 })
