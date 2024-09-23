ff9e3e8b77e5a17693898e3f60c23b77566b2cae
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-09-23 01:28:43 -0300

    feat: add onsubmit form login component

--- app/components/form-login.tsx
+++ app/components/form-login.tsx
@@ -6,8 +6,16 @@ import { loginSchema, loginSchemaType } from "@/schemas/loginSchema";
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import { loginAction } from "@/actions/auth-actions";
+import { useState, useTransition } from "react";
+import { useRouter } from "next/navigation";
 
 const FormLogin = () => {
+    const [isPending, startTransition] = useTransition();
+    const [error, setError] = useState<string | null>(null);
+    const router = useRouter();
+
     const form = useForm<loginSchemaType>({
         resolver: zodResolver(loginSchema),
         defaultValues: {
@@ -16,15 +24,24 @@ const FormLogin = () => {
         }
     });
 
-    const onSubmit = (values: loginSchemaType) => {
-       
+    const onSubmit = async (values: loginSchemaType) => {
+        setError(null);
+        startTransition(async () => {
+            const response = await loginAction(values);
+            console.log(response)
+            if (response.error) {
+                setError(response.error);
+            } else {
+                router.push("/dashboard");
+            }
+        });
     };
 
     return (
-        <div className="max-w-80 border p-5 border-black shadow-md rounded-md">
-            <h1 className="">Login</h1>
+        <div className="max-w-96 border px-12 py-14 border-gray-400 shadow-md rounded-md">
+          <h1 className="mb-5 font-bold">Login</h1>
             <Form {...form}>
-                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 ">
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                     <FormField control={form.control} name="email" render={({ field }) => (
                         <FormItem>
                             <FormLabel>
@@ -36,7 +53,7 @@ const FormLogin = () => {
                             <FormMessage />
                         </FormItem>
                     )} />
-                       <FormField control={form.control} name="password" render={({ field }) => (
+                    <FormField control={form.control} name="password" render={({ field }) => (
                         <FormItem>
                             <FormLabel>
                                 Senha
@@ -47,7 +64,11 @@ const FormLogin = () => {
                             <FormMessage />
                         </FormItem>
                     )} />
-                    <Button className="" type="submit">Iniciar sessão</Button>
+
+                    {error && <FormMessage>{error}</FormMessage>}
+                    <Button className="" type="submit" disabled={isPending}>
+                        Iniciar sessão
+                    </Button>
                 </form>
             </Form>
         </div>
